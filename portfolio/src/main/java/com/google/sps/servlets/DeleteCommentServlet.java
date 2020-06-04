c24012a6026c82073af8e31e06c9d15baae20c9e
dallen68 <46762316+dallen68@users.noreply.github.com>
2020-06-04 09:59:20 -0400

    Update DeleteCommentServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -18,11 +18,6 @@ import java.lang.Integer;
 
 @WebServlet("/delete-comment")
 public class DeleteCommentServlet extends HttpServlet {
-    
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-  }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
