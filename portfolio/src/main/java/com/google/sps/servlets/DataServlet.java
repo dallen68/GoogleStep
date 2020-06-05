0ed5c13c0da528c41a19570a4eaf591e6578257a
dallen68 <46762316+dallen68@users.noreply.github.com>
2020-06-05 11:36:53 -0400

    Update DataServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -31,7 +31,7 @@ import java.lang.Integer;
 import java.util.List;
 
 /**Servlet that handles comments **/
-@WebServlet("/data")
+@WebServlet("/comments")
 public class DataServlet extends HttpServlet {
     
   @Override
